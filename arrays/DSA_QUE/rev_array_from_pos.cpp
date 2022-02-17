66645015b7346036443020c70621f1008c89764c
PUSALA T R SATYA VANI <66248163+satya117@users.noreply.github.com>
2022-02-17 20:18:36 +0530

    Create rev_array_from_pos.cpp

---
+++ arrays/DSA_QUE/rev_array_from_pos.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void reversearray(vector<int> &arr,int m){
+    //using two pointer tech
+    int i=m,j=arr.size()-1;
+    while(i<=j){
+        swap(arr[i++],arr[j--]);
+    }
+    
+}
+
+int main()
+{
+    vector<int> arr = {1,2,3,4, 5,6,7,8};
+    int n = arr.size();
+    int m = 3; // position from where elements to reversed
+    cout<<"The dulicate ele: "<<endl;
+    reversearray(arr,m);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
